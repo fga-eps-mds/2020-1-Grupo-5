f2de736dd89fd0f8f6eb2aca8adee2fc36d84d20
Gabriel Azevedo Batalha <batalha800@gmail.com>
2020-10-26 17:38:59 -0300

    Formatado alguns tópicos com Tags HTML

--- src/tips.py
+++ src/tips.py
@@ -1,4 +1,4 @@
-from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 
 ENTRY_REGEX = '^(O que é|Prevenção|Sintomas|Transmissão|Suspeita|Fake news|Telefones|Locais)$'
 CHOOSING = 0
@@ -65,27 +65,28 @@ def symptoms(update, context):
     )
 def prevention(update, context):
 	text = ('Aqui você encontra informações sobre prevenção de agravos, diagnósticos, tratamentos, reabilitação e a manutenção da saúde. O Ministério da Saúde aconselha a população a tomar medidas preventivas de eficácia comprovada:\n\n'
-			'Higiene pessoal:\n\n'
-				'- Higienizar as mãos com água e sabão, ou com álcool gel, principalmente depois de tossir ou espirrar; de usar o banheiro; antes de comer, antes e depois de tocar os olhos, a boca e o nariz\n'
-				'- Evitar tocar os olhos, nariz ou boca após contato com superfícies potencialmente contaminadas, como corrimãos, bancos e maçanetas\n'
-				'- Evitar proteger a tosse e o espirro com as mãos, utilizando, preferencialmente, lenço de papel descartável\n'
-				'- Evitar contato com pessoas apresentem a síndrome gripa\n'
-				'- Assegurar que sua família esteja com a carteira de vacinação atualizada\n\n'
-				'Higiene do alimentos:\n\n'
-				'- Lavar bem frutas e verduras com água limpa e deixar de molho por alguns minutos em vinagre (1 colher por litro de água) ou hipoclorito de sódio (3 gotas por litro de água)\n'
-				'- Cobrir os alimentos para evitar que insetos entrem em contato\n'
-				'- Evitar compartilhar qualquer objeto que seja levado à boca\n\n'
-				'Higiene ambiental:\n\n'
-				'- Manter o quintal sempre limpo, roçando a grama/capim e podando as árvores\n'
-				'- Não jogar lixo no quintal. Acondicionar o lixo doméstico e colocar na frente da casa próximo aos horários de coleta, para evitar a presença de animais, como insetos e ratos\n'
-				'- Não queimar lixo\n'
-				'- Limpar e desinfetar superfícies regularmente\n'
-				'- Não fumar dentro de casa\n'
-				'- Eliminar possíveis criadouros de vetores de doenças\n'
-				'- Se não houver rede de esgoto na região, os banheiros devem ser construídos longe de poços d\'água, de nascentes ou da beira do rio')
+			'<b>Higiene pessoal:</b>\n\n'
+			'- Higienizar as mãos com água e sabão, ou com álcool gel, principalmente depois de tossir ou espirrar; de usar o banheiro; antes de comer, antes e depois de tocar os olhos, a boca e o nariz\n'
+			'- Evitar tocar os olhos, nariz ou boca após contato com superfícies potencialmente contaminadas, como corrimãos, bancos e maçanetas\n'
+			'- Evitar proteger a tosse e o espirro com as mãos, utilizando, preferencialmente, lenço de papel descartável\n'
+			'- Evitar contato com pessoas apresentem a síndrome gripa\n'
+			'- Assegurar que sua família esteja com a carteira de vacinação atualizada\n\n'
+			'<b>Higiene do alimentos:</b>\n\n'
+			'- Lavar bem frutas e verduras com água limpa e deixar de molho por alguns minutos em vinagre (1 colher por litro de água) ou hipoclorito de sódio (3 gotas por litro de água)\n'
+			'- Cobrir os alimentos para evitar que insetos entrem em contato\n'
+			'- Evitar compartilhar qualquer objeto que seja levado à boca\n\n'
+			'<b>Higiene ambiental:</b>\n\n'
+			'- Manter o quintal sempre limpo, roçando a grama/capim e podando as árvores\n'
+			'- Não jogar lixo no quintal. Acondicionar o lixo doméstico e colocar na frente da casa próximo aos horários de coleta, para evitar a presença de animais, como insetos e ratos\n'
+			'- Não queimar lixo\n'
+			'- Limpar e desinfetar superfícies regularmente\n'
+			'- Não fumar dentro de casa\n'
+			'- Eliminar possíveis criadouros de vetores de doenças\n'
+			'- Se não houver rede de esgoto na região, os banheiros devem ser construídos longe de poços d\'água, de nascentes ou da beira do rio')
 	context.bot.send_message(
 		chat_id=update.effective_chat.id,
 		text = text,
+		parse_mode=ParseMode.HTML
 	)
 
 def fake_news(update, context):
@@ -98,30 +99,32 @@ def fake_news(update, context):
 			'- Para áudios e vídeos, busque por palavras chave e tente encontrar a fonte\n'
 			'- Se uma pessoa te enviou, pergunte a ela sobre as fontes\n'
 			'- Sobre a covid-19 os seguintes sites possuem informação confiável:\n\n'
-			' 1- Ministério da Saúde\n'
-			' 2- Conselho Nacional de Secretarias de Saúde\n'
-			' 3- OPAS/OMS Brasil\n'	
-			' 4- Anvisa\n'
-			' 5- INCA\n'
-			' 6- Fiocruz\n'
-			' 7- Science Traslational Medicine')
+			'  <b>1.</b>Ministério da Saúde\n'
+			'  <b>2.</b>Conselho Nacional de Secretarias de Saúde\n'
+			'  <b>3.</b>OPAS/OMS Brasil\n'	
+			'  <b>4.</b>Anvisa\n'
+			'  <b>5.</b>INCA\n'
+			'  <b>6.</b>Fiocruz\n'
+			'  <b>7.</b><i>Science Traslational Medicine</i>')
 	context.bot.send_message(
 		chat_id=update.effective_chat.id,
 		text = text,
+		parse_mode=ParseMode.HTML
 	)
 
 def phone_numbers(update, context):
 	text = ('Lista de telefones úteis:\n\n'
-			'- Disque Saúde do Ministério da Saúde:\n(136)\n'
-			'- Corpo de Bombeiros:\n(193)\n'
-			'- SAMU:\n(192)\n'
-			'- Polícia Militar:\n(190)\n'			
-			'- Polícia Rodoviária Federal:\n(191)\n'
-			'- Poiícia Rodoviária Estadual:\n(198)\n'
-			'- Defesa Civil:\n(199)')
+			'- Disque Saúde do Ministério da Saúde:\n<b>(136)</b>\n'
+			'- Corpo de Bombeiros:\n<b>(193)</b>\n'
+			'- SAMU:\n<b>(192)</b>\n'
+			'- Polícia Militar:\n<b>(190)</b>\n'			
+			'- Polícia Rodoviária Federal:\n<b>(191)</b>\n'
+			'- Poiícia Rodoviária Estadual:\n<b>(198)</b>\n'
+			'- Defesa Civil:\n<b>(199)</b>')
 	context.bot.send_message(
 		chat_id=update.effective_chat.id,
 		text = text,
+		parse_mode=ParseMode.HTML
 	)
 
 def transmission(update, context):
