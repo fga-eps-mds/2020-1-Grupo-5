fec5273f04234a9e454ca296b251bf0d2cccf571
Gabriel Azevedo Batalha <batalha800@gmail.com>
2020-11-08 15:23:04 -0300

    Teste geraString adicionado

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,55 +2,65 @@ import unittest
 from src import utils
 
 class UtilsTests(unittest.TestCase):
+	def test_geraString(self):
+		text = {"user_name" : 'User', "gender" : 'Gender'}
+		# Certo
+		self.assertEqual("Atualmente essas são as suas informações: \n\nUsername: User\nGenero sexual: Gender", utils.geraString(text))
+		# Tópico a menos
+		self.assertNotEqual("Atualmente essas são as suas informações: \n\nUsername: User\n", utils.geraString(text))
+		# Informação errada
+		self.assertNotEqual("Atualmente essas são as suas informações: \n\nUsername: User0\nGenero sexual: Gender", utils.geraString(text))
+		# Tópico a mais
+		self.assertNotEqual("Atualmente essas são as suas informações: \n\nUsername: User\nGenero sexual: Gender\n Grupo de Risco: Sim", utils.geraString(text))
 
-    def test_remove_check_mark(self):
-        text = 'Senha✅'
-        self.assertEqual('Senha', utils.remove_check_mark(text))
+	def test_remove_check_mark(self):
+		text = 'Senha✅'
+		self.assertEqual('Senha', utils.remove_check_mark(text))
 
 
-    def test_update_check_mark(self):
-        keyboard = [['Username✅', 'Email'],
+	def test_update_check_mark(self):
+		keyboard = [['Username✅', 'Email'],
                     ['Senha', 'Raça'],
                     ['Trabalho✅', 'Genero sexual'],
                     ['Localização', 'Cancelar']]
-        category = 'Raça'
-        validation = True
-        utils.update_check_mark(keyboard, category, validation)
-        self.assertTrue(['Senha', 'Raça✅'] in keyboard)
-
-        category = 'Username'
-        validation = False
-        utils.update_check_mark(keyboard, category, validation)
-        self.assertFalse(['Username✅', 'Email'] in keyboard)
-
-
-    def test_update_required_data(self):
-        received_data = set(['Username', 'Email', 'Senha', 'Trabalho'])
-        required_data = {'Email', 'Raça', 'Genero sexual', 'Localização'}
-        utils.update_required_data(received_data, required_data)
-        self.assertFalse('Email' in required_data)
+		category = 'Raça'
+		validation = True
+		utils.update_check_mark(keyboard, category, validation)
+		self.assertTrue(['Senha', 'Raça✅'] in keyboard)
+		
+		category = 'Username'
+		validation = False
+		utils.update_check_mark(keyboard, category, validation)
+		self.assertFalse(['Username✅', 'Email'] in keyboard)
 
 
-    def test_form_filled(self):
-        keyboard = [['Email', 'Senha'],
+	def test_update_required_data(self):
+		received_data = set(['Username', 'Email', 'Senha', 'Trabalho'])
+		required_data = {'Email', 'Raça', 'Genero sexual', 'Localização'}
+		utils.update_required_data(received_data, required_data)
+		self.assertFalse('Email' in required_data)
+
+
+	def test_form_filled(self):
+		keyboard = [['Email', 'Senha'],
                     ['Cancelar']]
-        utils.form_filled(keyboard)
-        self.assertTrue(['Done'] in keyboard)
+		utils.form_filled(keyboard)
+		self.assertTrue(['Done'] in keyboard)
 
 
-    def test_undone_keyboard(self):
-        keyboard = [['Email', 'Senha'],
+	def test_undone_keyboard(self):
+		keyboard = [['Email', 'Senha'],
                     ['Cancelar'],
                     ['Done']]
-        utils.undone_keyboard(keyboard)
-        self.assertFalse(['Done'] in keyboard)
+		utils.undone_keyboard(keyboard)
+		self.assertFalse(['Done'] in keyboard)
 
 
-    def test_unreceived_info(self):
-        received_data = set(['Username', 'Email', 'Senha'])
-        required_data = {'Raça', 'Genero sexual'}
-        utils.unreceived_info(received_data, required_data, ("Username", "Email", "Senha","Raça", "Trabalho", "Genero sexual"))
-        self.assertTrue('Trabalho' in required_data)
+	def test_unreceived_info(self):
+		received_data = set(['Username', 'Email', 'Senha'])
+		required_data = {'Raça', 'Genero sexual'}
+		utils.unreceived_info(received_data, required_data, ("Username", "Email", "Senha","Raça", "Trabalho", "Genero sexual"))
+		self.assertTrue('Trabalho' in required_data)
 
 
 if __name__ == '__main__':
