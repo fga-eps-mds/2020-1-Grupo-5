2a80bdd9fae9610f77784ba4f82512da195903d8
lucas229 <lucasro229@gmail.com>
2020-11-27 19:08:49 -0300

    Correção de erro

--- tests/integration/tips.py
+++ tests/integration/tips.py
@@ -23,62 +23,62 @@ async def run_tests(client: Client):
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'O que é')  # clica em O que é
     assert response.num_messages == 2
-    assert 'Em dezembro de 2019 foi' in response.messages[0].text
+    assert 'Em dezembro de 2019 foi' in response.full_text
 
     print('Prevenção apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Prevenção')  # clica em Prevenção
     assert response.num_messages == 2
-    assert 'Higiene ambiental:' in response.messages[0].text
+    assert 'Higiene ambiental:' in response.full_text
 
     print('Sintomas apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Sintomas')  # clica em Sintomas
     assert response.num_messages == 2
-    assert 'Distúrbios gastrintestinais' in response.messages[0].text
+    assert 'Distúrbios gastrintestinais' in response.full_text
 
     print('Transmissão apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Transmissão')  # clica em Transmissão
     assert response.num_messages == 2
-    assert 'A transmissão pode ocorrer de uma' in response.messages[0].text
+    assert 'A transmissão pode ocorrer de uma' in response.full_text
 
     print('Suspeita apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Suspeita')  # clica em Suspeita
     assert response.num_messages == 2
-    assert 'postos de triagem nas Unidades Básicas' in response.messages[0].text
+    assert 'postos de triagem nas Unidades Básicas' in response.full_text
 
     print('(Fake news) apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Fake news')  # clica em Fake news
     assert response.num_messages == 2
-    assert 'Science Translational Medicine' in response.messages[0].text
+    assert 'Science Translational Medicine' in response.full_text
     
     print('Telefones apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Telefones')  # clica em Telefones
     assert response.num_messages == 2
-    assert 'Defesa Civil' in response.messages[0].text
+    assert 'Defesa Civil' in response.full_text
 
     print('Locais apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Locais')  # clica em Fontes
     assert response.num_messages == 2
-    assert 'Google Maps' in response.messages[0].text
+    assert 'Google Maps' in response.full_text
     assert response.inline_keyboards
 
     print('Fontes apertado')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Fontes')  # clica em Fontes
     assert response.num_messages == 2
-    assert '- BBC News' in response.messages[0].text
+    assert '- BBC News' in response.full_text
 
     print('Voltando ao menu inicial')
     async with controller.collect(count=2) as response:
         await client.send_message(controller.peer_id, 'Voltar')  # clica em Voltar
     assert response.num_messages == 2
-    assert 'Retornando' in response.messages[0].text
+    assert 'Retornando' in response.full_text
 
     print('Dicas testado com sucesso\n')
 
